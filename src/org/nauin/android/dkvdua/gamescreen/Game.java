ee1d9a1bbb64f64745fc2e45ef5205fc0ebae012
nauin <adnantoa@gmail.com>
2015-04-09 14:49:31 +0700

    Perubahan pada paket game screen

---
+++ src/org/nauin/android/dkvdua/gamescreen/Game.java
@@ -0,0 +1,67 @@
+package org.nauin.android.dkvdua.gamescreen;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+public abstract class Game implements ApplicationListener {
+	private Screen screen;
+
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		if (screen != null) {
+			screen.resize(width, height);
+		}
+	}
+
+	@Override
+	public void render() {
+		if (screen != null) {
+			screen.render(Gdx.graphics.getDeltaTime());
+		}
+	}
+
+	@Override
+	public void pause() {
+		if (screen != null) {
+			screen.pause();
+		}
+	}
+
+	@Override
+	public void resume() {
+		if (screen != null) {
+			screen.resume();
+		}
+	}
+
+	@Override
+	public void dispose() {
+		if (screen != null) {
+			screen.hide();
+		}
+	}
+
+	public Screen getScreen() {
+		return screen;
+	}
+
+	public void setScreen(Screen screen) {
+		if (this.screen != null) {
+			this.screen.hide();
+		}
+
+		this.screen = screen;
+
+		if (this.screen != null) {
+			this.screen.show();
+			this.screen.resize(Gdx.graphics.getWidth(),
+					Gdx.graphics.getHeight());
+		}
+	}
+}
---
+++ src/org/nauin/android/dkvdua/gamescreen/GameScreen.java
@@ -0,0 +1,61 @@
+package org.nauin.android.dkvdua.gamescreen;
+
+import org.nauin.android.dkvdua.gameworld.GameRenderer;
+import org.nauin.android.dkvdua.gameworld.GameWorld;
+import org.nauin.android.dkvdua.helper.InputHandler;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+	private GameWorld world;
+	private GameRenderer renderer;
+
+	public GameScreen() {
+		float screenWidth = Gdx.graphics.getWidth();
+		float screenHeight = Gdx.graphics.getHeight();
+		float gameWidth = 136;
+		float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+		world = new GameWorld(gameWidth, gameHeight);
+		Gdx.input.setInputProcessor(new InputHandler(world, screenWidth
+				/ gameWidth, screenHeight / gameHeight));
+		renderer = new GameRenderer(world, (int) gameWidth, (int) gameHeight);
+	}
+
+	@Override
+	public void render(float delta) {
+		world.update(delta);
+		renderer.render(delta);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
