4b98f9cb63e8b581de05d6ced7aaa0600ca24881
Adnanto A.R <adnantoa@gmail.com>
2015-06-03 21:11:40 +0700

    Perubahan pada kelas AndroidLauncher

--- src/org/nauin/android/dkvdua/AndroidLauncher.java
+++ src/org/nauin/android/dkvdua/AndroidLauncher.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+/**
+ * Kelas yang digunakan untuk memulai permainan, merupakan kelas utama
+ * 
+ * @author nauin
+ *
+ */
 public class AndroidLauncher extends AndroidApplication {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useWakelock = true;
 		initialize(new DKVDuaMain(), config);
 	}
 }
