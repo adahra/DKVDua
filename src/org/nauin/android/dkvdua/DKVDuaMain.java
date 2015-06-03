13b7860c7c69eee1b613907869036317052cb0b2
Adnanto A.R <adnantoa@gmail.com>
2015-06-03 21:11:18 +0700

    Perubahan pada kelas DKVDuaMain

--- src/org/nauin/android/dkvdua/DKVDuaMain.java
+++ src/org/nauin/android/dkvdua/DKVDuaMain.java
@@ -2,13 +2,20 @@ package org.nauin.android.dkvdua;
 
 import org.nauin.android.dkvdua.gameobject.AssetLoader;
 import org.nauin.android.dkvdua.gamescreen.Game;
+import org.nauin.android.dkvdua.layar.LayarMenuUtama;
 
+/**
+ * Kelas utama dari aplikasi
+ * 
+ * @author nauin
+ *
+ */
 public class DKVDuaMain extends Game {
 
 	@Override
 	public void create() {
 		AssetLoader.load();
-		setScreen(getScreen());
+		setScreen(new LayarMenuUtama(this));
 	}
 	
 	@Override
