ce76149a4398071cb8253bf3d0e5f2df2233f6be
Adnanto A.R <adnantoa@gmail.com>
2015-06-23 14:29:45 +0700

    Kelas coba-coba

---
+++ src/org/nauin/android/dkvdua/GdxTest.java
@@ -0,0 +1,33 @@
+package org.nauin.android.dkvdua;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.InputAdapter;
+
+public abstract class GdxTest extends InputAdapter implements
+		ApplicationListener {
+
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
