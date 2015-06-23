b7617aadfa5674864454b2ffae9b6eacad8a7f95
Adnanto A.R <adnantoa@gmail.com>
2015-06-23 14:32:59 +0700

    Perubahan pada kelas input handler

--- src/org/nauin/android/dkvdua/helper/InputHandler.java
+++ src/org/nauin/android/dkvdua/helper/InputHandler.java
@@ -1,7 +1,13 @@
 package org.nauin.android.dkvdua.helper;
 
-import org.nauin.android.dkvdua.gameworld.GameWorld;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.nauin.android.dkvdua.gameobject.AssetLoader;
+import org.nauin.android.dkvdua.gameworld.GameWorld;
+import org.nauin.android.dkvdua.ui.Tombol;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 
 /**
@@ -14,13 +20,38 @@ public class InputHandler implements InputProcessor {
 	private GameWorld myWorld;
 	private float scaleFactorX;
 	private float scaleFactorY;
-	
-	public InputHandler(GameWorld myWorld, float scaleFactorX, float scaleFactorY) {
+	private List<Tombol> tombolMenu;
+	private Tombol tombolMulai;
+	private Tombol tombolPengaturan;
+	private Tombol tombolKeluar;
+
+	public InputHandler(GameWorld myWorld, float scaleFactorX,
+			float scaleFactorY) {
 		this.myWorld = myWorld;
 		this.scaleFactorX = scaleFactorX;
 		this.scaleFactorY = scaleFactorY;
+
+		tombolMenu = new ArrayList<Tombol>();
+		tombolMulai = new Tombol((Gdx.graphics.getWidth() / 4) - 25,
+				(Gdx.graphics.getHeight() / 4) - 25,
+				(Gdx.graphics.getWidth() / 4), (Gdx.graphics.getHeight() / 4),
+				AssetLoader.dkvduaMatchButtonNormal,
+				AssetLoader.dkvduaMatchButtonClick);
+		tombolPengaturan = new Tombol((Gdx.graphics.getWidth() / 4) - 25,
+				(Gdx.graphics.getHeight() / 4) - 35,
+				(Gdx.graphics.getWidth() / 4), (Gdx.graphics.getHeight() / 4),
+				AssetLoader.dkvduaMatchButtonNormal,
+				AssetLoader.dkvduaMatchButtonClick);
+		tombolKeluar = new Tombol((Gdx.graphics.getWidth() / 4) - 25,
+				(Gdx.graphics.getHeight() / 4) - 45,
+				(Gdx.graphics.getWidth() / 4), (Gdx.graphics.getHeight() / 4),
+				AssetLoader.dkvduaMatchButtonNormal,
+				AssetLoader.dkvduaMatchButtonClick);
+		tombolMenu.add(tombolMulai);
+		tombolMenu.add(tombolPengaturan);
+		tombolMenu.add(tombolKeluar);
 	}
-	
+
 	@Override
 	public boolean keyDown(int keycode) {
 		return false;
@@ -61,4 +92,7 @@ public class InputHandler implements InputProcessor {
 		return false;
 	}
 
+	public List<Tombol> getTombolMenu() {
+		return tombolMenu;
+	}
 }
