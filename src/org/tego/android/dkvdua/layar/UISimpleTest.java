510f45d12dfd5dbbdb0defb09e3218f0ef317d27
Adnanto A.R <adnantoa@gmail.com>
2015-09-07 12:27:05 +0700

    Kelas java yang digunakan untuk test tampilan pada gim

--- src/org/tego/android/dkvdua/layar/UISimpleTest.java
+++ src/org/tego/android/dkvdua/layar/UISimpleTest.java
@@ -1,22 +1,21 @@
 package org.tego.android.dkvdua.layar;
 
+import org.tego.android.dkvdua.gameobject.AssetLoader;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 
 /**
  * Kelas yang digunakan untuk membuat/melakukan test terhadap layar yang dibuat
@@ -26,9 +25,60 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
  *
  */
 public class UISimpleTest implements Screen {
-	private Skin mSkin;
-	private Stage mStage;
-	private SpriteBatch mSpriteBatch;
+	private TiledMap map;
+	private MapLayers layers;
+	private TiledMapTileLayer layer;
+	private TiledMapRenderer renderer;
+	private Texture tiles;
+	private TextureRegion[][] splitTiles;
+	private Cell cell;
+	private SpriteBatch spriteBatch;
+	private OrthographicCamera oCamera;
+	private float lebar = Gdx.graphics.getWidth();
+	private float tinggi = Gdx.graphics.getHeight();
+	private int[][] arrayPetaHalangan = new int[][] {
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+			{ 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9 } };
+
+	private int[][] arrayPetaObyek = new int[][] {
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 
 	/*
 	 * (non-Javadoc)
@@ -39,8 +89,13 @@ public class UISimpleTest implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		mStage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-		mStage.draw();
+
+		oCamera.update();
+		renderer.setView(oCamera);
+		renderer.render();
+		spriteBatch.begin();
+
+		spriteBatch.end();
 	}
 
 	/*
@@ -50,7 +105,9 @@ public class UISimpleTest implements Screen {
 	 */
 	@Override
 	public void resize(int width, int height) {
-		mStage.getViewport().update(width, height, true);
+		// oCamera.viewportWidth = width;
+		// oCamera.viewportHeight = height;
+		// oCamera.update();
 	}
 
 	/*
@@ -60,43 +117,33 @@ public class UISimpleTest implements Screen {
 	 */
 	@Override
 	public void show() {
-		mSpriteBatch = new SpriteBatch();
-		mStage = new Stage();
-		Gdx.input.setInputProcessor(mStage);
+		oCamera = new OrthographicCamera();
+		oCamera.setToOrtho(false, (lebar / tinggi) * 320, 320);
+		oCamera.update();
 
-		mSkin = new Skin();
+		spriteBatch = new SpriteBatch();
 
-		Pixmap mPixmap = new Pixmap(1, 1, Format.RGBA8888);
-		mPixmap.setColor(Color.WHITE);
-		mPixmap.fill();
-		mSkin.add("white", new Texture(mPixmap));
-		mSkin.add("default", new BitmapFont());
+		// tiles = new Texture(Gdx.files.internal("data/gfx/dkvduaTextures.png"));
+		tiles = new Texture(Gdx.files.internal("data/gfx/dkvduaBasicTiles.png"));
+		splitTiles = TextureRegion.split(tiles, 16, 16);
 
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = mSkin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.down = mSkin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.checked = mSkin.newDrawable("white", Color.BLUE);
-		textButtonStyle.over = mSkin.newDrawable("white", Color.LIGHT_GRAY);
-		textButtonStyle.font = mSkin.getFont("default");
-		mSkin.add("default", textButtonStyle);
+		map = new TiledMap();
+		layers = map.getLayers();
 
-		Table mTable = new Table();
-		mTable.setFillParent(true);
-		mStage.addActor(mTable);
-
-		final TextButton button = new TextButton("Click Me!", mSkin);
-		mTable.add(button);
-
-		button.addListener(new ChangeListener() {
-
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				System.out.println("Clicked! Is checked: " + button.isChecked());
-				button.setText("Good job!");
+		for (int l = 0; l < 10; l++) {
+			layer = new TiledMapTileLayer(40, 20, 16, 16);
+			for (int x = 0; x < 150; x++) {
+				for (int y = 0; y < 100; y++) {
+					cell = new Cell();
+					cell.setTile(new StaticTiledMapTile(splitTiles[2][1]));
+					layer.setCell(x, y, cell);
+				}
 			}
-		});
+		}
 
-		mTable.add(new Image(mSkin.newDrawable("white", Color.RED))).size(64);
+		layers.add(layer);
+
+		renderer = new OrthogonalTiledMapRenderer(map);
 	}
 
 	/*
@@ -133,8 +180,7 @@ public class UISimpleTest implements Screen {
 	 */
 	@Override
 	public void dispose() {
-		mStage.dispose();
-		mSkin.dispose();
+
 	}
 
 }
