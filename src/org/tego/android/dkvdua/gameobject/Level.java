2476f3f4e326dda8e3526e50da90cd40b8ae1c3c
Adnanto A.R <adnantoa@gmail.com>
2015-09-14 11:26:11 +0700

    Perubahan pada kelas Level

--- src/org/tego/android/dkvdua/gameobject/Level.java
+++ src/org/tego/android/dkvdua/gameobject/Level.java
@@ -19,6 +19,8 @@
 
 package org.tego.android.dkvdua.gameobject;
 
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  * Kelas yang digunakan untuk menggambar peta level pada permainan
  * 
@@ -26,69 +28,58 @@ package org.tego.android.dkvdua.gameobject;
  *
  */
 public class Level {
-	private int[][] arrayPetaLantai = new int[][] {
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-			{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } };
-
-	private int[][] arrayPetaHalangan = new int[][] {
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
-			{ 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9 } };
-
-	private int[][] arrayPetaObyek = new int[][] {
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+	public static final int KOSONG = 0;
+	public static final int DINDING = 1;
+	public static final int KOTAK = 2;
+	public static final int LUBANG = 3;
+	public static final int LEBAR = 12;
+	public static final int TINGGI = 20;
+	public static final int UKURAN_UBIN = 16;
+	public int[][] data = new int[LEBAR][TINGGI];
+	public Rectangle kotak;
+	
+	
+	public Level() {
+		// TODO Auto-generated constructor stub
+		kotak = new Rectangle();
+		
+		for (int y = 0; y < TINGGI; y++) {
+			data[0][y] = DINDING;
+			data[LEBAR - 1][y] = DINDING;
+		}
+		
+		for (int x = 0; x < LEBAR; x++) {
+			if ((x > 0) && (x < LEBAR - 1)) {
+				data[x][10] = KOSONG;
+			}
+			
+			data[x][2] = DINDING;
+			data[x][4] = DINDING;
+			data[x][6] = DINDING;
+			data[x][8] = DINDING;
+			
+			data[x][0] = DINDING;
+			data[x][TINGGI - 1] = DINDING;
+		}
+		
+		data[10][2] = KOSONG;
+		data[1][4] = KOSONG;
+		data[10][6] = KOSONG;
+	}
+	
+	public void update(float delta) {
+		
+	}
+	
+	public boolean halangan(float x, float y) {
+		if (data[(int) x][(int) y] == DINDING) {
+			return true;
+		} else if (data[(int) x][(int) y] == KOTAK) {
+			return true;
+		} else if (data[(int) x][(int) y] == LUBANG) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
