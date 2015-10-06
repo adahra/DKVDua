9b7349f35980f85154c7f2d50054e52821388265
Adnanto A.R <adnantoa@gmail.com>
2015-10-06 20:07:23 +0700

    Membuat variabel konstan pada kelas

---
+++ src/org/tego/android/dkvdua/utilitas/Constants.java
@@ -0,0 +1,10 @@
+package org.tego.android.dkvdua.utilitas;
+
+public class Constants {
+	public static final float VIEWPORT_WIDTH = 1.0f;
+	public static final float VIEWPORT_HEIGHT = 1.0f;
+	public static final float VIEWPORT_GUI_WIDTH = 480.0f;
+	public static final float VIEWPORT_GUI_HEIGHT = 640.0f;
+	public static final String TEXTURE_ATLAS_OBJECTS = "data/gfx/texture/dkvdua-texture.atlas";
+	public static final int LIVES_START = 3;
+}
