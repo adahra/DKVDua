70a802b29a7e8d345a4cca222eadae84c22516da
Adnanto A.R <adnantoa@gmail.com>
2015-10-06 20:13:44 +0700

    Perubahan pada berkas kelas

--- src/org/tego/android/dkvdua/gamescreen/LayarPermainan.java
+++ src/org/tego/android/dkvdua/gamescreen/LayarPermainan.java
@@ -6,8 +6,6 @@ import org.tego.android.dkvdua.helper.InputHandler;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 
 /**
  * Kelas yang digunakan untuk mengatur penggambaran layar dari permainan
@@ -18,31 +16,20 @@ import com.badlogic.gdx.math.Vector2;
 public class LayarPermainan implements Screen {
 	private GameWorld world;
 	private GameRenderer renderer;
-	private static final int LEBAR_VIRTUAL = 320;
-	private static final int TINGGI_VIRTUAL = 480;
-	private static final float RASIO_ASPEK = (float) LEBAR_VIRTUAL / (float) TINGGI_VIRTUAL;
-	private Rectangle viewport;
 
 	public LayarPermainan() {
 		float screenWidth = Gdx.graphics.getWidth();
 		float screenHeight = Gdx.graphics.getHeight();
-		float gameWidth = 192;
+		float gameWidth = 320;
 		float gameHeight = screenHeight / (screenWidth / gameWidth);
-		// float gameHeight = 320; 
 
-		
-		
-		int nilaiTengah = (int) gameHeight / 2;
-		
 		world = new GameWorld(gameWidth, gameHeight);
 		Gdx.input.setInputProcessor(new InputHandler(world, screenWidth
 				/ gameWidth, screenHeight / gameHeight));
-		renderer = new GameRenderer(world, LEBAR_VIRTUAL, TINGGI_VIRTUAL);
-		
-		System.out.println("Lebar Layar HP  : " +screenWidth);
-		System.out.println("Tinggi Layar HP : " +screenHeight);
-		
-		System.out.println("Nilai Tengah    : " +nilaiTengah);
+		renderer = new GameRenderer(world, (int) gameWidth, (int) gameHeight);
+
+		System.out.println("Lebar Layar HP  : " + screenWidth);
+		System.out.println("Tinggi Layar HP : " + screenHeight);
 	}
 
 	/*
@@ -52,8 +39,6 @@ public class LayarPermainan implements Screen {
 	 */
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glViewport((int) viewport.x, (int) viewport.y, (int) viewport.width, (int) viewport.height);
-		
 		world.update(delta);
 		renderer.render(delta);
 	}
@@ -65,23 +50,7 @@ public class LayarPermainan implements Screen {
 	 */
 	@Override
 	public void resize(int width, int height) {
-		float rasioAspek = (float) width / (float) height;
-		float skala = 1f;
-		Vector2 crop = new Vector2(0f, 0f);
 		
-		if (rasioAspek > RASIO_ASPEK) {
-			skala = (float) height / (float) TINGGI_VIRTUAL;
-			crop.x = (width - LEBAR_VIRTUAL * skala) / 2f;
-		} else if (rasioAspek < RASIO_ASPEK) {
-			skala = (float) width / (float) LEBAR_VIRTUAL;
-			crop.y = (height - TINGGI_VIRTUAL * skala) / 2f;
-		} else {
-			skala = (float) width / (float) LEBAR_VIRTUAL;
-		}
-		
-		float w = (float) LEBAR_VIRTUAL * skala;
-		float h = (float) TINGGI_VIRTUAL * skala;
-		viewport = new Rectangle(crop.x, crop.y, w, h);
 	}
 
 	/*
