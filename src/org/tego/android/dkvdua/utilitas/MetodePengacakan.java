24062a8f2e23411813b2c534e7fcfd720fd8518c
Adnanto A.R <adnantoa@gmail.com>
2015-10-13 10:17:25 +0700

    Perubahan dan penambahan fungsi acak array dua dimensi

--- src/org/tego/android/dkvdua/utilitas/MetodePengacakan.java
+++ src/org/tego/android/dkvdua/utilitas/MetodePengacakan.java
@@ -17,14 +17,15 @@ public class MetodePengacakan {
 	}
 
 	/**
-	 * Method yang digunakan untuk melakukan pengacakan array
+	 * Method yang digunakan untuk melakukan pengacakan array dimensi satu
 	 * 
 	 * @param array
+	 *            Nilai dari array dimensi satu
 	 */
 	public static void shuffle(int[] array) {
 		int n = array.length;
 
-		while (n > 1) {
+		while (n > 0) {
 			int k = MathUtils.random(n--);
 			int temp = array[n];
 			array[n] = array[k];
@@ -33,15 +34,34 @@ public class MetodePengacakan {
 	}
 
 	/**
-	 * Method yang digunakan untuk melakukan pengacakan array yang berbentuk
-	 * obyek
+	 * Method yang digunakan untuk melakukan pengacakan array dimensi dua
 	 * 
 	 * @param array
+	 *            Nilai dari array dimensi dua
+	 */
+	public static void shuffle(int[][] array) {
+		for (int x = 0; x < array.length; x++) {
+			for (int y = 0;  y < array[0].length; y++) {
+				int i = MathUtils.random(x);
+				int j = MathUtils.random(y);
+				int temp = array[x][y];
+				array[x][y] = array[i][j];
+				array[i][j] = temp;
+			}
+		}
+	}
+
+	/**
+	 * Method yang digunakan untuk melakukan pengacakan array dimensi satu yang
+	 * berbentuk obyek
+	 * 
+	 * @param array
+	 *            Nilai dari array dimensi satu
 	 */
 	public static void shuffle(Object[] array) {
 		int n = array.length;
 
-		while (n > 1) {
+		while (n > 0) {
 			int k = MathUtils.random(n--);
 			Object temp = array[n];
 			array[n] = array[k];
