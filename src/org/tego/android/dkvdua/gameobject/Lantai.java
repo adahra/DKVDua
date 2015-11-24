abfd0ad3490dfe5a5f3a2483a862ea162dae10d2
Adnanto A.R <adnantoa@gmail.com>
2015-11-24 15:36:58 +0700

    Perubahan pada berkas Kelas Lantai

---
+++ src/org/tego/android/dkvdua/gameobject/Lantai.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.tego.android.dkvdua.gameobject;
+
+/**
+ * @author blackshadow
+ *
+ */
+public class Lantai extends Obyek {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2180043764991078856L;
+	public static final String TAG = Lantai.class.getSimpleName();
+	
+	public Lantai(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+}
