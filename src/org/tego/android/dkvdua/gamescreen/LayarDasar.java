3815cdb6435a0b874dc896c89a887da6e90c860c
Adnanto A.R <adnantoa@gmail.com>
2015-12-27 16:25:09 +0700

    Kelas yang digunakan untuk membuat kelas layar

---
+++ src/org/tego/android/dkvdua/gamescreen/LayarDasar.java
@@ -0,0 +1,182 @@
+/**
+ * 
+ */
+package org.tego.android.dkvdua.gamescreen;
+
+import org.tego.android.dkvdua.DKVDuaMain;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * @author blackshadow
+ *
+ */
+public class LayarDasar implements InputProcessor, Screen {
+	private float lebar;
+	private float tinggi;
+	
+	protected DKVDuaMain dkvduaMain = null;
+	
+	/**
+	 * 
+	 */
+	public LayarDasar(DKVDuaMain dkvduaMain) {
+		// TODO Auto-generated constructor stub
+		this.dkvduaMain = dkvduaMain;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#render(float)
+	 */
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resize(int, int)
+	 */
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		lebar = width;
+		tinggi = height;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#show()
+	 */
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		lebar = Gdx.graphics.getWidth();
+		tinggi = Gdx.graphics.getHeight();
+		Gdx.input.setInputProcessor(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#hide()
+	 */
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		Gdx.input.setInputProcessor(null);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#pause()
+	 */
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resume()
+	 */
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#dispose()
+	 */
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		Gdx.input.setInputProcessor(null);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#keyDown(int)
+	 */
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#keyUp(int)
+	 */
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#keyTyped(char)
+	 */
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#touchDown(int, int, int, int)
+	 */
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#touchUp(int, int, int, int)
+	 */
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#touchDragged(int, int, int)
+	 */
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#mouseMoved(int, int)
+	 */
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.badlogic.gdx.InputProcessor#scrolled(int)
+	 */
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public float getLebar() {
+		return lebar;
+	}
+	
+	public float getTinggi() {
+		return tinggi;
+	}
+	
+	public DKVDuaMain getGim() {
+		return dkvduaMain;
+	}
+}
